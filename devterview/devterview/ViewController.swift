45da1aeeadc60f4e5a466d190ad2e3374e3c7772
compuTasha <mijookim.tasha@gmail.com>
2023-07-24 16:45:10 +0900

    [Setting] Xcode 프로젝트 생성 (#3)

---
+++ devterview/devterview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  devterview
+//
+//  Created by Mijoo Kim on 2023/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
