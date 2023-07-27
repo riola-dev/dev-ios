e7e8bb2e04bbd388dbedab98ac6ea9068c7dcb1e
GODNOEL <ah2650@naver.com>
2023-07-27 15:53:32 +0900

    [Add] DevterviewMainViewController 생성 (#6)

---
+++ devterview/devterview/Screens/DevterviewMain/DevterviewMainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DevterviewMainViewController.swift
+//  devterview
+//
+//  Created by Yu ahyeon on 2023/07/27.
+//
+
+import UIKit
+
+class DevterviewMainViewController: UIViewController {
+
+    // MARK: - Property
+    private var songList: [String] = ["Computer Architecture", "Data Structure", "Algorithm", "Database", "Network & Security",
+                                      "Operating System", "Design Pattern"]
+    
+    // MARK: - View
+    
+    
+    // MARK: - Init
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .purple
+    }
+    
+    // MARK: - Method
+
+}
