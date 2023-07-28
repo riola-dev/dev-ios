ba386463a2c527d7b5ed99e11f978b3ffe8ca9d5
GODNOEL <ah2650@naver.com>
2023-07-28 15:23:03 +0900

    [Add] Component+Size 파일 추가 (#7)
     Component+Size를 저장하고 사용하기 위함

---
+++ devterview/devterview/Global/Utils/Component+Size.swift
@@ -0,0 +1,12 @@
+//
+//  Component+Size.swift
+//  devterview
+//
+//  Created by Yu ahyeon on 2023/07/28.
+//
+
+import UIKit
+
+struct BasicComponentSize {
+    static let width = UIScreen.main.bounds.width - 32
+}
