74e9040d594e42fc48084a6c1428efe8fb2252d0
GODNOEL <ah2650@naver.com>
2023-08-01 16:18:10 +0900

    [FIx] collectionVIew header 추가 (#12)

--- devterview/devterview/Screens/DevterviewResult/Component/DevterviewHistoryCollectionViewCell.swift
+++ devterview/devterview/Screens/DevterviewResult/Component/DevterviewHistoryCollectionViewCell.swift
@@ -31,7 +31,7 @@ final class DevterviewHistoryCollectionViewCell: UICollectionViewCell {
     private let questionContentLabel: BasicLabel = {
         $0.numberOfLines = 0
         return $0
-    }(BasicLabel(contentText: "", fontStyle: .content02Regular, textColorInfo: .white))
+    }(BasicLabel(contentText: "", fontStyle: .content01Regular, textColorInfo: .white))
     
     private let separatorLineView: UIView = {
         $0.frame = CGRect(x: 0, y:0, width: 100, height: 1)
@@ -71,8 +71,8 @@ final class DevterviewHistoryCollectionViewCell: UICollectionViewCell {
     }(UIStackView(arrangedSubviews: [answerTitleLabel, moveAnswerLabel]))
     
     private let answerTitleLabel = BasicLabel(contentText: "내 답변 보기",
-                                             fontStyle: .caption,
-                                             textColorInfo: .white)
+                                              fontStyle: .content02Light,
+                                              textColorInfo: .white)
     
     private let moveAnswerLabel: UIImageView = {
         $0.image = ImageLiteral.chevronRightSymbol
@@ -81,7 +81,6 @@ final class DevterviewHistoryCollectionViewCell: UICollectionViewCell {
         return $0
     }(UIImageView())
     
-    private let spaceView = UIView()
     
     // MARK: - Init
 
@@ -129,3 +128,49 @@ final class DevterviewHistoryCollectionViewCell: UICollectionViewCell {
 }
 
 
+// MARK: - HistoryCollectionViewHeader
+
+final class HistoryCollectionViewHeader: UICollectionReusableView {
+    
+    // MARK: - Property
+    
+    static let identifier = "HistoryCollectionViewHeader"
+    
+    // MARK: - View
+    
+    private lazy var resultScoreUIView = ResultScoreUIView(score: "40점")
+    
+    private let myDevterviewTitleLabel = BasicLabel(contentText: "내 뎁터뷰",
+                                                    fontStyle: .content01Bold,
+                                                    textColorInfo: .white)
+    
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setupLayout()
+ 
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Method
+    
+    private func setupLayout() {
+        self.addSubview(resultScoreUIView)
+        resultScoreUIView.constraint(top: self.topAnchor,
+                                     leading: self.leadingAnchor,
+                                     trailing: self.trailingAnchor)
+        
+        self.addSubview(myDevterviewTitleLabel)
+        myDevterviewTitleLabel.constraint(top: resultScoreUIView.bottomAnchor,
+                                          padding: UIEdgeInsets(top: 40, left: 0, bottom: 0, right:0))
+        
+    }
+    
+    func configure(score: String) {
+       lazy var resultScoreUIView = ResultScoreUIView(score: score)
+    }
+}
--- devterview/devterview/Screens/DevterviewResult/DevterviewResultViewController.swift
+++ devterview/devterview/Screens/DevterviewResult/DevterviewResultViewController.swift
@@ -9,48 +9,33 @@ import UIKit
 
 class DevterviewResultViewController: UIViewController {
     
-    
     // MARK: - Property
     
     private var questionTest = [ "Linked List의 개념을 설명할수 있습니까?array에 비해 Linked List을 사용하는 장점과 단점은 무엇입니까?", "dfskldfdkjfskljfslkdfjkldjfksjfkl","Linked List의 개념을 설명할수 있습니까?array에 비해 Linked List을 사용하는 장점과 단점은 무엇입니까?", "dfskldfdkjfskljfslkdfjkldjfksjfkl","Linked List의 개념을 설명할수 있습니까?array에 비해 Linked List을 사용하는 장점과 단점은 무엇입니까?", "dfskldfdkjfskljfslkdfjkldjfksjfkl" ]
     
-    //MARK: - scrollView
-    
-    private lazy var scrollView: UIScrollView = {
-        $0.showsVerticalScrollIndicator = true
-        return $0
-    }(UIScrollView())
-    
-    private let contentView = UIView()
-    
     // MARK: - View
-    
-    private lazy var resultScoreUIView = ResultScoreUIView(score: "40점", resultText: "어쩌구저쩌구")
-    
-    private let myDevterviewLabel = BasicLabel(contentText: "내 뎁터뷰",
-                                               fontStyle: .content01Bold,
-                                               textColorInfo: .white)
     
     private let collectionViewFlowLayout: UICollectionViewFlowLayout = {
         $0.scrollDirection = .vertical
-        $0.itemSize = CGSize(width: BasicComponentSize.width, height: 150)
-//            $0.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        $0.itemSize = CGSize(width: BasicComponentSize.width, height: 160)
         $0.minimumLineSpacing = 10
+        $0.headerReferenceSize = CGSize(width: BasicComponentSize.width, height: 220)
         return $0
     }(UICollectionViewFlowLayout())
     
     private lazy var devterviewHistoryCollectionView = {
+        $0.dataSource = self
+        $0.delegate = self
+        $0.backgroundColor = .clear
         $0.register(
             DevterviewHistoryCollectionViewCell.self,
             forCellWithReuseIdentifier: "DevterviewHistoryCollectionViewCell"
         )
-        $0.isScrollEnabled = false
-        $0.dataSource = self
-        $0.delegate = self
-        $0.backgroundColor = .mainPink
+        $0.register(HistoryCollectionViewHeader.self,
+                    forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                    withReuseIdentifier: HistoryCollectionViewHeader.identifier)
         return $0
     }(UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout))
-    
     
     // MARK: - LifeCycle
     
@@ -63,34 +48,12 @@ class DevterviewResultViewController: UIViewController {
     
     private func setupLayout() {
         
-        view.addSubview(scrollView)
-        scrollView.constraint(top: view.safeAreaLayoutGuide.topAnchor,
-                              leading: view.safeAreaLayoutGuide.leadingAnchor,
-                              bottom: view.bottomAnchor,
-                              trailing: view.safeAreaLayoutGuide.trailingAnchor,
-                              padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
-        
-        scrollView.addSubview(contentView)
-        contentView.constraint(top: scrollView.contentLayoutGuide.topAnchor,
-                               leading: scrollView.contentLayoutGuide.leadingAnchor,
-                               bottom: scrollView.contentLayoutGuide.bottomAnchor,
-                               trailing: scrollView.contentLayoutGuide.trailingAnchor)
-        
-        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
-        
-        self.view.addSubview(resultScoreUIView)
-        resultScoreUIView.constraint(top: contentView.topAnchor,
-                                     leading: contentView.leadingAnchor,
-                                     trailing: contentView.trailingAnchor,
-                                     padding: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
-        
         self.view.addSubview(devterviewHistoryCollectionView)
-        devterviewHistoryCollectionView.constraint(top: resultScoreUIView.bottomAnchor,
-                                                   leading: contentView.leadingAnchor,
-                                                   bottom: contentView.bottomAnchor,
-                                                   trailing: contentView.trailingAnchor,
-                                                   padding: UIEdgeInsets(top: 30, left: 16, bottom: 0, right: 16))
-        devterviewHistoryCollectionView.constraint(.heightAnchor, constant: 1500)
+        devterviewHistoryCollectionView.constraint(top: view.safeAreaLayoutGuide.topAnchor,
+                                                   leading: view.safeAreaLayoutGuide.leadingAnchor,
+                                                   bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                                   trailing: view.safeAreaLayoutGuide.trailingAnchor,
+                                                   padding: UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16))
     }
     
 }
@@ -99,7 +62,7 @@ class DevterviewResultViewController: UIViewController {
 
 // TODO - 카테고리 선택 시 터치이벤트 추가 필요
 extension DevterviewResultViewController: UICollectionViewDelegate {
-
+    
 }
 
 // MARK: - UICollectionViewDataSource
@@ -110,8 +73,9 @@ extension DevterviewResultViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         5
     }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(
             withReuseIdentifier: "DevterviewHistoryCollectionViewCell",
             for: indexPath) as? DevterviewHistoryCollectionViewCell
@@ -120,9 +84,20 @@ extension DevterviewResultViewController: UICollectionViewDataSource {
         }
         cell.configure(question: questionTest[indexPath.item],
                        score: "10점")
-        
         return cell
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        viewForSupplementaryElementOfKind kind: String,
+                        at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        guard kind == UICollectionView.elementKindSectionHeader,
+              let header = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: HistoryCollectionViewHeader.identifier,
+                for: indexPath
+              ) as? HistoryCollectionViewHeader else {return UICollectionReusableView()}
+        
+        return header
+    }
 }
-
