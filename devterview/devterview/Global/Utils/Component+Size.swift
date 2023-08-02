4b7d7e72f7db386e0de46aeb71c8392a78c13b34
GODNOEL <ah2650@naver.com>
2023-08-02 16:37:41 +0900

    [Add] Component+Size에 SFIconSize 추가 (#18)

--- devterview/devterview/Global/Utils/Component+Size.swift
+++ devterview/devterview/Global/Utils/Component+Size.swift
@@ -10,3 +10,7 @@ import UIKit
 struct BasicComponentSize {
     static let width = UIScreen.main.bounds.width - 32
 }
+
+struct SFIconSize {
+    static let regularIconSize = UIImage.SymbolConfiguration(pointSize: 40)
+}
