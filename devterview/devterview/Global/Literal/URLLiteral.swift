2da361b34873ce7c776414fc0ad045063628b042
GODNOEL <ah2650@naver.com>
2023-08-03 13:45:25 +0900

    [Add] URL 링크 URLLiteral로 이동 (#18)

--- devterview/devterview/Global/Literal/URLLiteral.swift
+++ devterview/devterview/Global/Literal/URLLiteral.swift
@@ -12,6 +12,10 @@ enum URLLiteral {
     // MARK: - API
     static let chatGPTURL: String  = "https://api.openai.com/v1/chat/completions"
     
-    // MARK: - notion
+    // MARK: - SettingViewController
+    
+    static let privacyPolicyNotionURL = "https://chain-hyssop-308.notion.site/11698780785747f182077d329163574a?pvs=4"
+    static let thirdPartyInfoNotionURL = "https://chain-hyssop-308.notion.site/a7e8bd0c250e43da80396f1ffeea507f?pvs=4"
+    static let supportDeveloperURL = "https://devterview.postype.com/post/15025004"
     
 }
